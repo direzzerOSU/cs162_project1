3dc45a95b3394f63d54f0ae2992a7f7b74f0f490
Ryan DiRezze <direzzer@oregonstate.edu>
2018-10-07 18:48:35 -0700

    program runs on FLIP & reflection drafted with flowchart & initial tests

--- inputValidation.hpp
+++ inputValidation.hpp
@@ -6,10 +6,11 @@ using std::cout;
 using std::cin;
 using std::endl;
 #include<string>
+#include<limits>
 
 // works cited
    // (1) http://www.cplusplus.com/reference/istream/istream/ignore/
-   
+
 void inputValidation(int&);
 
 #endif
